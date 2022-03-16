ff5b4ed9c82f4329962a496764c0800530e17f75
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-03-16 14:02:41 +0100

    fix: resolve GStringImpl.trim()

---
+++ src/test/resources/io/gravitee/policy/groovy/play_with_strings.groovy
@@ -0,0 +1,9 @@
+def prefix = "prefix";
+def separator = "_";
+
+def getContent(prefix, separator, suffix) {
+    def content = "${prefix}${separator}${suffix}"
+    return content.trim();
+}
+
+return getContent(prefix, separator, 'suffix')
