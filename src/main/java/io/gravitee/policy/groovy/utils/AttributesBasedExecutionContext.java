fb3706b1a7016f6229de992026a85362293f2cb0
Guillaume Lamirand <guillaume.lamirand@graviteesource.com>
2024-11-06 15:41:50 +0100

    fix: use latest node and apim version
    BREAKING CHANGE: Tracer interface is not more available through tracer-api module

--- src/main/java/io/gravitee/policy/groovy/utils/AttributesBasedExecutionContext.java
+++ src/main/java/io/gravitee/policy/groovy/utils/AttributesBasedExecutionContext.java
@@ -19,7 +19,6 @@ import io.gravitee.el.TemplateEngine;
 import io.gravitee.gateway.api.ExecutionContext;
 import io.gravitee.gateway.api.Request;
 import io.gravitee.gateway.api.Response;
-import io.gravitee.tracing.api.Tracer;
 import java.util.Enumeration;
 import java.util.Map;
 
